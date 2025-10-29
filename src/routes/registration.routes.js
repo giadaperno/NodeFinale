9fbe892abc2e55747da61801e1f5a274de119873
giadaperno <giada.perno@edu-its.it>
2025-10-29 15:07:51 +0100

    completato b3

--- src/routes/registration.routes.js
+++ src/routes/registration.routes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { registerToEvent } from "../controllers/registration.controller.js";
+import { registerToEvent, cancelRegistration } from "../controllers/registration.controller.js";
 import { verifyToken } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
+// Iscrizione a un evento
 router.post("/", verifyToken, registerToEvent);
 
+// Annulla iscrizione a un evento
+router.delete("/", verifyToken, cancelRegistration);
 export default router;
